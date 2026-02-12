79a008f8759f43eebb7d9652fce97da04c702b4f
Tyler <tfriesen2@my.gcu.edu>
2026-02-12 12:57:21 -0700

    added optimized image from angular to the home page

--- frontend/angular_frontend/src/app/home-page/home-page.component.ts
+++ frontend/angular_frontend/src/app/home-page/home-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { NgOptimizedImage } from '@angular/common';
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NgOptimizedImage],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  title = 'Incident Response Simulator';
+  heroText = 'Compelling marketing copy blah blah blah';
+  ctaButtonText = "Let's Go!";
 
+  onCTAButtonClick() {
+    // TODO: fill this method out
+  }
 }
