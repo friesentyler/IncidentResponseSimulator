27869c7f0ea69254c3ddf42f24afc9557988efac
Tyler <tfriesen2@my.gcu.edu>
2026-02-13 22:24:16 -0700

    added input fields to register card and link to login if already registered

--- frontend/angular_frontend/src/app/register-page/register-page.component.ts
+++ frontend/angular_frontend/src/app/register-page/register-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterLink],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
   cardTitle: string = 'Create a new account';
-  cardText: string = 'Enter your email below to create a new account';
+  cardText: string = 'Enter your email below to create a new account. If you already have an account';
 }
