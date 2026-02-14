fa1917b2a5c00ca58341bac7ced2684acec4b2b1
Tyler <tfriesen2@my.gcu.edu>
2026-02-13 23:11:03 -0700

    login component completed

---
+++ frontend/angular_frontend/src/app/login-page/login-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-login-page',
+    imports: [NavbarComponent, RouterLink, ReactiveFormsModule],
+    templateUrl: './login-page.component.html',
+    styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+    private fb = inject(FormBuilder);
+
+    cardTitle: string = 'Login to your account';
+    cardText: string = 'Enter your email below to login to your account. If you don\'t have an account yet';
+    submitted = false;
+
+    loginForm: FormGroup = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]]
+    });
+
+    onSubmit() {
+        this.submitted = true;
+        if (this.loginForm.valid) {
+            console.log('Login Attempt!', this.loginForm.value);
+            // TODO: Implement actual login logic
+        }
+    }
+}
