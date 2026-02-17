679523ffb2e68a996a7a6d16ee5b8f30750b29ff
Tyler <tfriesen2@my.gcu.edu>
2026-02-17 10:05:38 -0700

    added provideRouter to all tests to stop failures, removed some outdated tests

--- frontend/angular_frontend/src/app/register-page/register-page.component.spec.ts
+++ frontend/angular_frontend/src/app/register-page/register-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { RegisterPageComponent } from './register-page.component';
 
 describe('RegisterPageComponent', () => {
@@ -8,16 +8,27 @@ describe('RegisterPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterPageComponent]
+      imports: [RegisterPageComponent],
+      providers: [provideRouter([])],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RegisterPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+  // test valid email and password validation combo and that the error messages and DOM elements don't appear
+  // test invalid email but valid password email should have error message password is fine
+  // test valid email but invalid password that error messages appear
+  // test invalid email and invalid password and that error messages appear
+  // test that login button works
+  // test that register button works
+  it('should submit form properly on click register button', () => {
+    spyOn(component, 'onSubmit');
+    const button: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('.nb-button.blue');
+    button.click();
+    expect(component.onSubmit).toHaveBeenCalled()
+  })
 });
--- frontend/angular_frontend/src/app/shared/navbar/navbar.component.spec.ts
+++ frontend/angular_frontend/src/app/shared/navbar/navbar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -8,9 +8,10 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [NavbarComponent],
+      providers: [provideRouter([])],
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
