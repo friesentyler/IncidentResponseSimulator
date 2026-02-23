826a10318b40e54d32ca01a6579fcd50b0f02a24
Tyler <tfriesen2@my.gcu.edu>
2026-02-22 19:12:32 -0700

    broke scenario button out into its own component from dashboard

--- frontend/angular_frontend/src/app/dashboard-page/dashboard-page.component.ts
+++ frontend/angular_frontend/src/app/dashboard-page/dashboard-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ScenarioButtonComponent } from '../shared/scenario-button/scenario-button.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [ScenarioButtonComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
