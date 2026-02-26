e4b7050928598f4cf8fbed72203b39e14eea431d
Tyler <tfriesen2@my.gcu.edu>
2026-02-26 16:00:24 -0700

    angular tests broke due to duplicate import

--- frontend/angular_frontend/src/app/app.component.spec.ts
+++ frontend/angular_frontend/src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
